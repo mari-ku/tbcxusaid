bad1ad6d29f35d500a09552eb03cd8461b5c414e
Marie <kutubidze.mariam2@gmail.com>
2024-01-25 19:10:20 +0400

    finished styling questions section

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function toggleSection(header) {
-    var content = header.nextElementSibling;
-    if (content.style.maxHeight === '0px') {
-        content.style.maxHeight = content.scrollHeight + 'px';
-        content.style.display = 'block'; // Show the content
-    } else {
-        content.style.maxHeight = '0px';
-        content.style.display = 'none'; // Hide the content
-    }
-}
-
-
 function toggleAnswer(question) {
     var answer = question.nextElementSibling;
     var arrow = question.querySelector('.arrow');
