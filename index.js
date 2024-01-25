fa20df78ea2227342dde01b55d66bed85bb1aede
Marie <kutubidze.mariam2@gmail.com>
2024-01-26 02:16:24 +0400

    added partners' slider. styling is not finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function toggleAnswer(question) {
-    var answer = question.nextElementSibling;
-    var arrow = question.querySelector('.arrow');
+    let answer = question.nextElementSibling;
+    let arrow = question.querySelector('.arrow');
     
     if (answer.style.display === 'block') {
         answer.style.display = 'none';
@@ -8,5 +8,74 @@ function toggleAnswer(question) {
     } else {
         answer.style.display = 'block';
         arrow.classList.add('open');
+    }
+}
+
+function fadeIn(index){
+    document.getElementById(`slide-${index}`).style.opacity = 1;
+}
+
+function fadeOut(index){
+    document.getElementById(`slide-${index}`).style.opacity = 0;
+}
+
+let sliderIndex = 0;
+
+var sliderInterval = setInterval(() => {
+    if(sliderIndex !== 2) {
+        fadeOut(sliderIndex);
+        sliderIndex = sliderIndex + 1;
+        fadeIn(sliderIndex);
+    } else {
+        fadeOut(sliderIndex);
+        sliderIndex = 0;
+        fadeIn(sliderIndex);
+    }
+},4000)
+
+document.addEventListener('DOMContentLoaded', function() {
+    fadeIn(0);
+    document.getElementById('slider').addEventListener('mouseenter', () => {
+        clearInterval(sliderInterval);
+    })
+    
+    document.getElementById('slider').addEventListener('mouseleave', () => {
+        sliderInterval = setInterval(() => {
+            if(sliderIndex !== 2) {
+                fadeOut(sliderIndex);
+                sliderIndex = sliderIndex + 1;
+                fadeIn(sliderIndex);
+            } else {
+                fadeOut(sliderIndex);
+                sliderIndex = 0;
+                fadeIn(sliderIndex);
+            }
+        },4000)
+    })
+});
+
+
+
+function prevSlide() {
+    if(sliderIndex !== 0) {
+        fadeOut(sliderIndex);
+        sliderIndex = sliderIndex - 1;
+        fadeIn(sliderIndex);
+    } else {
+        fadeOut(sliderIndex);
+        sliderIndex = 2;
+        fadeIn(sliderIndex);
+    }
+}
+
+function nextSlide() {
+    if(sliderIndex !== 2) {
+        fadeOut(sliderIndex);
+        sliderIndex = sliderIndex + 1;
+        fadeIn(sliderIndex);
+    } else {
+        fadeOut(sliderIndex);
+        sliderIndex = 0;
+        fadeIn(sliderIndex);
     }
 }
