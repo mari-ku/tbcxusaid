af167f82a752c9b6e3f3d92a8db05d13db7fbed6
Marie <kutubidze.mariam2@gmail.com>
2024-01-27 00:13:08 +0400

    added sidebar and finished styling the sidebar

--- index.js
+++ index.js
@@ -52,6 +52,10 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         },4000)
     })
+
+    document.getElementById('policies').addEventListener('click', toggleMenu)
+
+    document.getElementById('closemenu').addEventListener('click', closeMenu)
 });
 
 
@@ -78,4 +82,29 @@ function nextSlide() {
         sliderIndex = 0;
         fadeIn(sliderIndex);
     }
+}
+
+
+function toggleMenu() {
+    var sideMenu = document.getElementById('sideMenu');
+    var overlay = document.getElementById('overlay');
+
+    // Toggle the menu and overlay visibility
+    sideMenu.style.right = (sideMenu.style.right === '0px') ? '-250px' : '0';
+    overlay.style.display = (overlay.style.display === 'block') ? 'none' : 'block';
+
+    // Toggle the background color of the body
+    document.body.style.background = (document.body.style.background === 'rgba(0, 0, 0, 0.5)') ? '#fff' : 'rgba(0, 0, 0, 0.5)';
+}
+
+function closeMenu() {
+    var sideMenu = document.getElementById('sideMenu');
+    var overlay = document.getElementById('overlay');
+
+    // Close the menu and overlay
+    sideMenu.style.right = '-50%';
+    overlay.style.display = 'none';
+
+    // Restore the background color of the body
+    document.body.style.background = '#fff';
 }
